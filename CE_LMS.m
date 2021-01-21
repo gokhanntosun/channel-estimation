2e025208db5b045a0386a0a4e83d22dab6c5136e
Gökhan <gokhantosun@live.com>
2021-01-21 21:12:38 +0300

    number of training iterations corrected

--- CE_LMS.m
+++ CE_LMS.m
@@ -1,12 +1,13 @@
 clear;close all;
 h=[0.74 -0.514 0.37 0.216 0.062];%given channel statistics
 N=10000;%block length
-T=length(h);%10;%tap count
+H=length(h);%lenght of channel response
 frame_cnt=1000;%frame count
 Ks=[5 10 20];%pilot count
 SNR_dB=0:2:20;%snr values in db
 SNR=1./(2*(10.^(SNR_dB./10)));%snr values in bit energy
-BER_RLS=zeros(1,length(SNR_dB));%ber vs snr vector
+MSE_LMS=zeros(length(Ks),length(SNR_dB));%avg mse matrix
+h_LMS=zeros(H,length(Ks));%estimated channel parameters
 mu=0.25;%step size constant
 %--------------------------------------------------------------------------
 %iterations----------------------------------------------------------------
@@ -15,26 +16,25 @@ for i=1:length(Ks)%pilot count loop
     for s=1:length(SNR_dB)%snr loop
         %------------------------------------------------------------------
         %definitions for recursion-----------------------------------------
-        h_est=zeros([T 1]);%channel impulse to be estimated
+        h_est=zeros([H 1]);%channel impulse to be estimated
         ld=1;%exponential forget factor
         ldi=1/ld;%inverse of forget factor, for ease
-        u=zeros(T,1);%observation vector
-        d=zeros(T,1);%known data vector
-        P=SNR_dB(s)*10*eye(T);%inverse of autocorrelation
-        var=SNR(s);
-        fr=1;fer=0;
+        u=zeros(H,1);%observation vector
+        d=zeros(H,1);%known data vector
+        P=SNR_dB(s)*10*eye(H);%inverse of autocorrelation
+        var=SNR(s);%sigma squared for noise
+        fr=1;fer=0;%initialize
         %------------------------------------------------------------------
-        %begin sim---------------------------------------------------------
+        %training----------------------------------------------------------
         while fr<frame_cnt
-            %--------------------------------------------------------------
-            %training part-------------------------------------------------
             x_p=randi([0 1],[1 K]);x_p(x_p==0)=-1;%generate pilot symbols
             noise=normrnd(0,sqrt(var),[1,K+length(h)-1]);%noise samples
             y=conv(x_p,h)+noise;%impose channel conditions
+            x_p=[x_p zeros(1,H-1)];%add zero padding to the end
             %begin recursion
-            for n=1:K-T+1
-                u=fliplr(y(n:n+T-1))';%observation
-                e=y(n+T-1)-fliplr(x_p(:,n:n+T-1))*h_est;%calculate err
+            for n=1:K
+                u=fliplr(y(n:n+H-1))';%observation
+                e=y(n+H-1)-fliplr(x_p(:,n:n+H-1))*h_est;%calculate err
                 ss=mu/(u'*u);%compute step size for the current time instant
                 h_est=h_est+ss*e*u;%update eq coeffs
             end%end training
@@ -42,9 +42,11 @@ for i=1:length(Ks)%pilot count loop
             fer=fer+mse;%update frame error, accumulate mse
             fr=fr+1;%increase frame count
         end
-        MSE_LMS(i,s)=fer/fr;%average over frame count
-        [SNR_dB(s) fr fer]%print the current parameters
+        MSE_LMS(i,s)=fer/(K*fr);%average over frame count
+        disp([SNR_dB(s) fr fer]);%print the current parameters
     end%end snr loop
-    h_LMS(i,:)=h_est;%save final estimations 
+    h_est=h_est';
+    h_LMS(:,i)=h_est;%save final estimations 
 end%end pilot count loop
-save('CE_LMS','MSE_LMS','SNR_dB','h_LMS','h');
+%save results
+save('CE_LMS','MSE_LMS','h_LMS');
